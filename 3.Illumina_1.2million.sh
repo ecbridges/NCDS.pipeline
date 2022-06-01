6fec9f28321e556f85691035491c58e6d9f155c2
ecbridges <s1986958@ed.ac.uk>
2022-06-01 17:46:56 +0100

    completed Illumina 1.2million step three

--- 3.Illumina_1.2million.sh
+++ 3.Illumina_1.2million.sh
@@ -5,3 +5,29 @@
 #Requirements
 #	Plink 1.90b4
 #	Illumina 1.2 million array updated to build GRCh37 (name illumina.1.2.QC4)
+
+#Ensure PAR variants are properly assigned.
+plink --bfile illumina.1.2.QC4 --split-x b37 no-fail --make-bed --out illumina.1.2.QC5
+
+#Set remaining .hh instances to missing.
+plink --bfile illumina.1.2.QC5 --set-hh-missing --make-bed illumina.1.2.QC6
+
+#Sex check on pruned dataset.
+plink --bfile illumina.1.2.QC6 --indep-pairwise 1000 5 0.2 --out illumina.1.2.QC6
+plink --bfile illumina.1.2.QC6 --extract illumina.QC6.prune.in --sex-check --out illumina.1.2.sex
+grep PROBLEM illumina.1.2.sex.sexcheck >> illumina.1.2.sex.probs.txt
+
+#Remove indiivduals with sex errors.
+plink --brfile illumina.1.2.QC6 --remove illumina.1.2.sex.probs.txt --make-bed --out illumina.1.2.QC7
+
+#Check for related individuals.
+plink --bfile illumina.1.2.QC7 --indep-pairwise 1500 150 0.2 --out illumina.1.2.QC7
+plink --bfile illumina.1.2.QC7 --extract illumina.1.2.QC7.prune.in --make-bed --out pruned.illumina.1.2.QC7
+plink --bfile pruned.illumina.1.2.QC7 --genome --out illumina.1.2
+awk '$10 >= 0.1875 {print}' illumina.1.2.genome > illumina.1.2.related.txt
+
+#Check each related pair of individuals. Create text file, illumina.1.2.related.excl.txt, containing FID
+#and IID of each individual to be removed.
+plink --bfile illumina.1.2.QC7 --remove illumina.1.2.related.excl.txt --make-bed --out illumina.1.2.QC8
+
+
