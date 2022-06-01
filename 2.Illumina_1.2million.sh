be0dbe9b617054b995e663b9400223dd18516888
ecbridges <s1986958@ed.ac.uk>
2022-06-01 17:24:50 +0100

    completed illumina 1.2milion step 2

---
+++ 2.Illumina_1.2million.sh
@@ -0,0 +1,5 @@
+#This is the second step for the Illumina 1.2 million array preparation.
+#This step updates the genome build to GRCh37. In order to do this, follow the instructions at
+https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
+#For the Illumina 1.2 million array, you need to use the following strand file: 
+# Human1-2M-DuoCustom_v1_A ILMN
