fe1809f5764f4ae47ddd928baceb6e184d75174c
ecbridges <s1986958@ed.ac.uk>
2022-06-01 17:28:25 +0100

    updated illumina 1.2 step 2 with name of new file

--- 2.Illumina_1.2million.sh
+++ 2.Illumina_1.2million.sh
@@ -3,3 +3,4 @@
 https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
 #For the Illumina 1.2 million array, you need to use the following strand file: 
 # Human1-2M-DuoCustom_v1_A ILMN
+# Name the new, updated file illumina.1.2.QC4
---
+++ 3.Illumina_1.2million.sh
@@ -0,0 +1,7 @@
+#This is step 3 for the preparation of the Illumina 1.2 million array. This section will clean up
+#the updated dataset by addressing any sex errors, related individuals and heterozygrous haploid
+#loci.
+
+#Requirements
+#	Plink 1.90b4
+#	Illumina 1.2 million array updated to build GRCh37 (name illumina.1.2.QC4)
