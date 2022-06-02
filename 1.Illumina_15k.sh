35bf1a102188562722354c3642f8a5a7a3b157cc
ecbridges <s1986958@ed.ac.uk>
2022-06-02 10:46:19 +0100

    completed step one for illumina 15k

--- 1.Illumina_15k.sh
+++ 1.Illumina_15k.sh
@@ -15,35 +15,30 @@ plink --bfile illumina.15 --missing --out illumina.15.calls
 ##Remove SNPs with call rate < 0.97
 plink --bfile illumina.15 --geno 0.03 --make-bed --out illumina.15.QC1
 
-##Identify variants not in HWE.
-plink --bfile illumina.15.QC1 --hardy --out illumina.15
-awk '$3 == "UNAFF" && $9 < 0.000001' illumina.15.hwe | wc -l 
-#No variants fail HWE in this dataset.
-
 ##Identify individuals with high proportion of missing data.
 plink --bfile illumia.15.QC1 --missing --out illumina.15.calls.2
 awk '$6 >= 0.02 {print}' illumina.15.calls.2.imiss >> illumina.15.drop.txt
 plink --bfile illumina.15.QC1 --remove illumina.15.drop.txt --make-bed --out illumina.15.QC2
 
 ##Check for unexpected levels of heterozygostiy - calculate heterozygostiy proportions.
-plink --bfile illumina.15.QC2 --het --out illumina.1.2
-plink --bfile illumina.1.2.QC2 --missing --out illumina.1.2.calls.3
-echo "FID IID obs_HOM N_SNPs prop_HET" > illumina.1.2.prop.het.txt
-awk 'NR>1{print $1,$2,$3,$5,($5-$3)/$5}' illumina.1.2.het >> illumina.1.2.prop.het.txt
+plink --bfile illumina.15.QC2 --het --out illumina.15
+plink --bfile illumina.15.QC2 --missing --out illumina.15.calls.3
+echo "FID IID obs_HOM N_SNPs prop_HET" > illumina.15.prop.het.txt
+awk 'NR>1{print $1,$2,$3,$5,($5-$3)/$5}' illumina.15.het >> illumina.15.prop.het.txt
 
 ##Calculate plus and minus 3SD for heterozygosity
 R
-hetsd<- read.table("illumina.1.2.prop.het.txt", header = T)
+hetsd<- read.table("illumina.15.prop.het.txt", header = T)
 sd<- sd(hetsd[,5])
 hetmean<- mean(hetsd[,5])
 lbound<- hetmean - (sd*3)
 ubound<- hetmean + (sd*3)
 Stat<- c("SD2, "Lower Bound", "Uppder Bound", "Mean")
 Values<- c(sd, lbound, ubound, hetmean)
-illumina.1.2.sd<- data.frame(Stat, Values)
-write.table(illumina.1.2.sd, file = "illumina.1.2.sd.txt", row.names = F, quote = F, sep = "\t")
+illumina.15.sd<- data.frame(Stat, Values)
+write.table(illumina.15.sd, file = "illumina.15.sd.txt", row.names = F, quote = F, sep = "\t")
 q()
 
 ##Note upper and lower bound, named x and y respectively here. Remove heterozygosity outliers.
-'awk $5 <= y || $5 >= x' illumina.1.2.prop.het.txt >> illumina.1.2.het.drop.txt
-plink --bfile illumina.1.2.QC2 --remove illumina.1.2.het.drop.txt --make-bed --out illumina.1.2.QC3
+'awk $5 <= y || $5 >= x' illumina.15.prop.het.txt >> illumina.15.het.drop.txt
+plink --bfile illumina.15.QC2 --remove illumina.15.het.drop.txt --make-bed --out illumina.15.QC3
