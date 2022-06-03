cfbaae6d610e96ebfd1c6503d1253e975ba480b9
ecbridges <s1986958@ed.ac.uk>
2022-06-03 10:17:28 +0100

    infin 1 step 3 complete

--- 2.Illumina_15k.sh
+++ 2.Illumina_15k.sh
@@ -1,5 +1,7 @@
 #This is the second step for the Illumina 15k array preparation.
 #This step updates the genome build to GRCh37. In order to do this, follow the instructions at
 https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
+#For this array, the update process must be carried out twice. The first time, update to GRCh36. Then,
+#update the GRCh36 file to GRCh37.
 #For the Illumina 15k array, a custom file is required. See the webpage above for information on requesting custom files. 
-# Name the new, updated file illumina.15.QC4y
+# Name the new, updated GRCh37 file illumina.15.QC4
---
+++ 3.Infinium_550k_v1.1.sh
@@ -0,0 +1,22 @@
+#This is step 3 for the preparation of the Infinium 550k v1.1  array. This section will clean up
+#the updated dataset by addressing any sex errors, related individuals and heterozygrous haploid
+#loci.
+
+#Requirements
+#	Plink 1.90b4
+#	Infinium 550k v1.1 array updated to build GRCh37 (name infin.1.QC5)
+
+#Ensure PAR variants are properly assigned.
+plink --bfile infin.1.QC5 --split-x b37 no-fail --make-bed --out infin.1.QC6
+
+#Set remaining .hh instances to missing.
+plink --bfile infin.1.QC6 --set-hh-missing --make-bed infin.1.QC7
+
+#Sex check on pruned dataset.
+plink --bfile infin.1.QC7 --indep-pairwise 1000 5 0.2 --out infin.1.QC7
+plink --bfile infin.1.QC7 --extract infin.1.QC7.prune.in --sex-check --out infin.1.sex
+grep PROBLEM infin.1.sex.sexcheck >> infin.1.sex.probs.txt
+
+#Remove individuals with sex errors.
+plink --brfile infin.1.QC7 --remove infin.1.sex.probs.txt --make-bed --out infin.1.QC8
+
