a92107042cc2cccc9fdf14575b4f1b658188f595
ecbridges <s1986958@ed.ac.uk>
2022-06-03 09:47:29 +0100

    completed illuma 15k step 2

--- 2.Illumina_15k.sh
+++ 2.Illumina_15k.sh
@@ -1,5 +1,5 @@
 #This is the second step for the Illumina 15k array preparation.
 #This step updates the genome build to GRCh37. In order to do this, follow the instructions at
 https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
-#For the Illumina 15k array, a custom file is required. You can access this by emailing 
-# Name the new, updated file illumina.1.2.QC4
+#For the Illumina 15k array, a custom file is required. See the webpage above for information on requesting custom files. 
+# Name the new, updated file illumina.1.2.QC4.
