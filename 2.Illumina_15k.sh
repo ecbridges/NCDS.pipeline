9a608fe21b9bd8c09c9a1f9515aa850d85b513ef
ecbridges <s1986958@ed.ac.uk>
2022-06-03 10:10:24 +0100

    fixed typos

--- 2.Illumina_15k.sh
+++ 2.Illumina_15k.sh
@@ -2,4 +2,4 @@
 #This step updates the genome build to GRCh37. In order to do this, follow the instructions at
 https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
 #For the Illumina 15k array, a custom file is required. See the webpage above for information on requesting custom files. 
-# Name the new, updated file illumina.1.2.QC4.
+# Name the new, updated file illumina.15.QC4y
