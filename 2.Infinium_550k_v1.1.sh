ace6a9188c2426adc5d66ece0c7cf27b1d418214
ecbridges <s1986958@ed.ac.uk>
2022-06-03 10:09:25 +0100

    completed infinium v1 step 2

---
+++ 2.Infinium_550k_v1.1.sh
@@ -0,0 +1,6 @@
+#This is the second step for the Infinium 550k v1.1 array preparation.
+#This step updates the genome build to GRCh37. In order to do this, follow the instructions at
+https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
+#For the Infinium 550k v1.1 array, you need to use the following strand file: 
+# BDCHP-1X10-HUMANHAP550_11218540_C SRC
+# Name the new, updated file infin.1.QC5
