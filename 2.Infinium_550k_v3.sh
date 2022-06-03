1f9ba63e4e05864bd4c33937e9bf7b93fcf9baff
ecbridges <s1986958@ed.ac.uk>
2022-06-03 10:56:33 +0100

    infinium v3 step 2 complete

---
+++ 2.Infinium_550k_v3.sh
@@ -0,0 +1,6 @@
+#This is the second step for the Infinium 550k v3 array preparation.
+#This step updates the genome build to GRCh37. In order to do this, follow the instructions at
+https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
+#For the Infinium 550k v3 array, you need to use the following strand file: 
+# HumanHap550-2v3_B ILMN
+# Name the new, updated file infin.3.QC5
