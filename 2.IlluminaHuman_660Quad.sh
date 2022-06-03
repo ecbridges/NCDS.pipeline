636c0e09937c3c00923ed585dfc8976e1f6c03c8
ecbridges <s1986958@ed.ac.uk>
2022-06-03 17:34:38 +0100

    quad step 2 complete

---
+++ 2.IlluminaHuman_660Quad.sh
@@ -0,0 +1,6 @@
+#This is the second step for the Illumina Human 660-Quad array preparation.
+#This step updates the genome build to GRCh37. In order to do this, follow the instructions at
+https://www.well.ox.ac.uk/~wrayner/strand/ for the update_build.pl script.
+#For the Illumina Human 660-Quad array, you need to use the following strand file: 
+# Human610-Quadv1_B SRC
+# Name the new, updated file quad.QC4
