ebf1666bb62a984aac3762e7f7ad731e2654e1d8
ecbridges <s1986958@ed.ac.uk>
2022-06-03 16:56:17 +0100

    QC8 affymet 500k done step 3

---
+++ 3.Affymetrix_500k.sh
@@ -0,0 +1,13 @@
+#This is step 3 for the preparation of the Infinium 500k array. This section will clean up
+#the updated dataset by addressing any sex errors, related individuals and heterozygrous haploid
+#loci.
+
+#Requirements
+#	Plink 1.90b4
+#	Infinium 550k v3 array updated to build GRCh37 (name affy.500.QC6)
+
+#Ensure PAR variants are properly assigned.
+plink --bfile affy.500.QC6 --split-x b37 no-fail --make-bed --out affy.500.QC7
+
+#Set remaining .hh instances to missing.
+plink --bfile affy.500.QC7 --set-hh-missing --make-bed affy.500.QC8
