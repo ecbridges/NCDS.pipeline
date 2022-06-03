cbd0e60a2047c89c8f19f1acf9074f008a083e9c
ecbridges <s1986958@ed.ac.uk>
2022-06-03 11:00:37 +0100

    infinium v3 step 3 complete

---
+++ 3.Infinium_550k_v3.sh
@@ -0,0 +1,22 @@
+#This is step 3 for the preparation of the Infinium 550k v3  array. This section will clean up
+#the updated dataset by addressing any sex errors, related individuals and heterozygrous haploid
+#loci.
+
+#Requirements
+#	Plink 1.90b4
+#	Infinium 550k v3 array updated to build GRCh37 (name infin.3.QC5)
+
+#Ensure PAR variants are properly assigned.
+plink --bfile infin.3.QC5 --split-x b37 no-fail --make-bed --out infin.3.QC6
+
+#Set remaining .hh instances to missing.
+plink --bfile infin.3.QC6 --set-hh-missing --make-bed infin.3.QC7
+
+#Sex check on pruned dataset.
+plink --bfile infin.3.QC7 --indep-pairwise 1000 5 0.2 --out infin.3.QC7
+plink --bfile infin.3.QC7 --extract infin.3.QC7.prune.in --sex-check --out infin.3.sex
+grep PROBLEM infin.3.sex.sexcheck >> infin.3.sex.probs.txt
+
+#Remove individuals with sex errors.
+plink --brfile infin.3.QC7 --remove infin.3.sex.probs.txt --make-bed --out infin.3.QC8
+
