49e69b630daeb990c7f954f64da085baee653a74
ecbridges <s1986958@ed.ac.uk>
2022-07-04 11:18:36 +0100

    edited Quad chip to add merge-x

--- 3.IlluminaHuman_660Quad.sh
+++ 3.IlluminaHuman_660Quad.sh
@@ -12,22 +12,25 @@ plink --bfile quad.QC4 --split-x b37 no-fail --make-bed --out quad.QC5
 #Set remaining .hh instances to missing.
 plink --bfile quad.QC5 --set-hh-missing --make-bed quad.QC6
 
+#Re-merge PAR region.
+plink --bfile quad.QC6 --merge-x no-fail --make-bed --out quad.QC7
+
 #Sex check on pruned dataset.
-plink --bfile quad.QC6 --indep-pairwise 1000 5 0.2 --out quad.QC6
-plink --bfile quad.QC6 --extract quad.QC6.prune.in --sex-check --out quad.sex
+plink --bfile quad.QC7 --indep-pairwise 1000 5 0.2 --out quad.QC7
+plink --bfile quad.QC7 --extract quad.QC7.prune.in --check-sex --out quad.sex
 grep PROBLEM quad.sex.sexcheck >> quad.sex.probs.txt
 
 #Remove individuals with sex errors.
-plink --brfile quad.QC6 --remove quad.sex.probs.txt --make-bed --out quad.QC7
+plink --bfile quad.QC7 --remove quad.sex.probs.txt --make-bed --out quad.QC8
 
 #Check for related individuals.
-plink --bfile quad.QC7 --indep-pairwise 1500 150 0.2 --out quad.QC7
-plink --bfile quad.QC7 --extract quad.QC7.prune.in --make-bed --out pruned.quad.QC7
-plink --bfile pruned.quad.QC7 --genome --out quad
+plink --bfile quad.QC8 --indep-pairwise 1500 150 0.2 --out quad.QC8
+plink --bfile quad.QC8 --extract quad.QC8.prune.in --make-bed --out pruned.quad.QC8
+plink --bfile pruned.quad.QC8 --genome --out quad
 awk '$10 >= 0.1875 {print}' quad.genome > quad.related.txt
 
 #Check each related pair of individuals. Create text file, quad.related.excl.txt, containing FID
 #and IID of each individual to be removed.
-plink --bfile quad.QC7 --remove quad.related.excl.txt --make-bed --out quad.QC8
+plink --bfile quad.QC8 --remove quad.related.excl.txt --make-bed --out quad.QC9
 
 
--- 4.IlluminaHuman_660Quad.sh
+++ 4.IlluminaHuman_660Quad.sh
@@ -9,23 +9,23 @@
 
 #At this stage you may carry out principal components analysis for ancestry on the dataset if you 
 #will be needing these values. 
-plink --bfile quad.QC8 --indep-pairwise 1000 5 0.2 --out quad.QC8
-plink --bfile quad.QC8 --extract quad.QC8.prune.in --make-bed --pca header tabs --out quad.pca
+plink --bfile quad.QC9 --indep-pairwise 1000 5 0.2 --out quad.QC9
+plink --bfile quad.QC9 --extract quad.QC9.prune.in --make-bed --pca header tabs --out quad.pca
 
 #Genetate allele frequencies from clean file.
-plink --bfile quad.QC8 --freq --out quad
+plink --bfile quad.QC9 --freq --out quad
 
 #Now check data using Will Rayner's checking script. Script and associated files are available here: 
 #	https://www.well.ox.ac.uk/~wrayner/tools/
 #Run using code:
-perl HRC-1000G-check-bim.pl -b quad.QC8.bim -f quad.frq -r HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h
+perl HRC-1000G-check-bim.pl -b quad.QC9.bim -f quad.frq -r HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h
 
 #Run the output script, Run-plink.sh, to clean the file.
 
 #Convert all files to VCF format
 for CHR in {1..23};
 do
-plink --bfile quad.QC8-updated-chr${CHR} --recode vcf --out quad.updated.chr${CHR}
+plink --bfile quad.QC9-updated-chr${CHR} --recode vcf --out quad.updated.chr${CHR}
 bcftools sort quad.updated.chr${CHR}.vcf -O z -o quad.chr${CHR}.vcf.gz
 done
 
