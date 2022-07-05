9c36ba90dfccddfd14500c98548261a0668179b7
ecbridges <s1986958@ed.ac.uk>
2022-07-05 21:47:32 +0100

    illumina 1.2m file 1 corrections

--- 1.Illumina_1.2million.sh
+++ 1.Illumina_1.2million.sh
@@ -1,3 +1,4 @@
+plink --bfile illumia.1.2.QC1 --missing --out illumina.1.2.calls.2
 ##This file includes the first steps for conducting the quality control pipeline to upload the Illumina 1.2 million array from the
 ##NCDS to the Michigan Imputation Server.
 
@@ -16,7 +17,7 @@ plink --bfile illumina.1.2 --missing --out illumina.1.2.calls
 plink --bfile illumina.1.2 --geno 0.03 --make-bed --out illumina.1.2.QC1
 
 ##Identify individuals with high proportion of missing data.
-plink --bfile illumia.1.2.QC1 --missing --out illumina.1.2.calls.2
+plink --bfile illumina.1.2.QC1 --missing --out illumina.1.2.calls.2
 awk '$6 >= 0.02 {print}' illumina.1.2.calls.2.imiss >> illumina.1.2.drop.txt
 plink --bfile illumina.1.2.QC1 --remove illumina.1.2.drop.txt --make-bed --out illumina.1.2.QC2
 
@@ -33,12 +34,12 @@ sd<- sd(hetsd[,5])
 hetmean<- mean(hetsd[,5])
 lbound<- hetmean - (sd*3)
 ubound<- hetmean + (sd*3)
-Stat<- c("SD2, "Lower Bound", "Uppder Bound", "Mean")
+Stat<- c("SD", "Lower Bound", "Uppqer Bound", "Mean")
 Values<- c(sd, lbound, ubound, hetmean)
 illumina.1.2.sd<- data.frame(Stat, Values)
 write.table(illumina.1.2.sd, file = "illumina.1.2.sd.txt", row.names = F, quote = F, sep = "\t")
 q()
 
 ##Note upper and lower bound, named x and y respectively here. Remove heterozygosity outliers.
-'awk $5 <= y || $5 >= x' illumina.1.2.prop.het.txt >> illumina.1.2.het.drop.txt
+awk '$5 <= 0.288 || $5 >= 0.303' illumina.1.2.prop.het.txt >> illumina.1.2.het.drop.txt
 plink --bfile illumina.1.2.QC2 --remove illumina.1.2.het.drop.txt --make-bed --out illumina.1.2.QC3
