46b7445ab243b8bed50f2ea41f802146ead9dfb3
ecbridges <s1986958@ed.ac.uk>
2022-07-05 22:26:07 +0100

    corrections on illumina 1.2 file 3

--- 3.Illumina_1.2million.sh
+++ 3.Illumina_1.2million.sh
@@ -10,24 +10,31 @@
 plink --bfile illumina.1.2.QC4 --split-x b37 no-fail --make-bed --out illumina.1.2.QC5
 
 #Set remaining .hh instances to missing.
-plink --bfile illumina.1.2.QC5 --set-hh-missing --make-bed illumina.1.2.QC6
+plink --bfile illumina.1.2.QC5 --set-hh-missing --make-bed --out illumina.1.2.QC6
+
+#Ensure chromosomes are properly coded.
+plink --bfile illumina.1.2.QC6 --merge-x --make-bed --out illumina.1.2.QC7plink --bfile illumina.1.2.QC6 --merge-x --make-bed --out illumina.1.2.QC7
 
 #Sex check on pruned dataset.
-plink --bfile illumina.1.2.QC6 --indep-pairwise 1000 5 0.2 --out illumina.1.2.QC6
-plink --bfile illumina.1.2.QC6 --extract illumina.1.2.QC6.prune.in --sex-check --out illumina.1.2.sex
+plink --bfile illumina.1.2.QC7 --indep-pairwise 1000 5 0.2 --out illumina.1.2.QC7
+plink --bfile illumina.1.2.QC7 --extract illumina.1.2.QC7plink --bfile illumina.1.2.QC7 --indep-pairwise 1000 5 0.2 --out illumina.1.2.QC7
+plink --bfile illumina.1.2.QC7 --extract illumina.1.2.QC7.prune.in --check-sex --out illumina.1.2.sex
+grep PROBLEM illumina.1.2.sex.sexcheck >> illumina.1.2.sex.probs.txt
+.prune.in --check-sex --out illumina.1.2.sex
 grep PROBLEM illumina.1.2.sex.sexcheck >> illumina.1.2.sex.probs.txt
 
 #Remove individuals with sex errors.
-plink --brfile illumina.1.2.QC6 --remove illumina.1.2.sex.probs.txt --make-bed --out illumina.1.2.QC7
+plink --bfile illumina.1.2.QC7 --remove illumina.1.2.sex.probs.txt --make-bed --out illumina.1.2.QC8
 
 #Check for related individuals.
-plink --bfile illumina.1.2.QC7 --indep-pairwise 1500 150 0.2 --out illumina.1.2.QC7
-plink --bfile illumina.1.2.QC7 --extract illumina.1.2.QC7.prune.in --make-bed --out pruned.illumina.1.2.QC7
-plink --bfile pruned.illumina.1.2.QC7 --genome --out illumina.1.2
+plink --bfile illumina.1.2.QC8 --indep-pairwise 1500 150 0.2 --out illumina.1.2.QC8
+plink --bfile illumina.1.2.QC8 --extract illumina.1.2.QC8.prune.in --make-bed --out pruned.illumina.1.2.QC8
+plink --bfile pruned.illumina.1.2.QC8 --genome --out illumina.1.2
 awk '$10 >= 0.1875 {print}' illumina.1.2.genome > illumina.1.2.related.txt
 
 #Check each related pair of individuals. Create text file, illumina.1.2.related.excl.txt, containing FID
 #and IID of each individual to be removed.
-plink --bfile illumina.1.2.QC7 --remove illumina.1.2.related.excl.txt --make-bed --out illumina.1.2.QC8
+plink --bfile illumina.1.2.QC8 --remove illumina.1.2.related.excl.txt --make-bed --out illumina.1.2.QC9
 
 
+ 
