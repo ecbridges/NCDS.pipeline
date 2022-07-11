ba4adf509d51379dac6e67e9d07ebb3b90557798
ecbridges <s1986958@ed.ac.uk>
2022-07-11 16:45:15 +0100

    1.infin v3 corrections

--- 1.Infinium_550k_v3.sh
+++ 1.Infinium_550k_v3.sh
@@ -1,3 +1,7 @@
+plink --bfile infin.3 --geno 0.03 --make-bed --out infin.3.QC1
+plink --bfile infin.3.QC1 --hardy --out infin.3
+awk '$3 == "UNAFF" && $9 < 0.000001' infin.3.hwe | wc -l
+plink --bfile infin.3.QC1 --hwe 0.000001 --make-bed --out infin.3.QC2
 ##This file includes the first steps for conducting the quality control pipeline to upload the Infinium 550k v3 array from the
 ##NCDS to the Michigan Imputation Server.
 
@@ -29,7 +33,12 @@ plink --bfile infin.3.QC2 --remove infin.3.drop.txt --make-bed --out infin.3.QC3
 plink --bfile infin.3.QC3 --het --out infin.3.QC3
 plink --bfile infin.3.QC3 --missing --out infin.3.calls.3
 echo "FID IID obs_HOM N_SNPs prop_HET" > infin.3.prop.het.txt
-awk 'NR>1{print $1,$2,$3,$5,($5-$3)/$5}' infin.3.het >> infin.3.prop.het.txt
+awk 'NR>1{print $1,$2,$3,$5,($5-$3)/$5}' infin.3.QC3.hetsd<- read.table("infin.3.prop.het.txt", header = T)
+sd<- sd(hetsd[,5])
+hetmean<- mean(hetsd[,5])
+lbound<- hetmean - (sd*3)
+ubound<- hetmean + (sd*3)
+het >> infin.3.prop.het.txt
 
 ##Calculate plus and minus 3SD for heterozygosity
 R
@@ -38,12 +47,12 @@ sd<- sd(hetsd[,5])
 hetmean<- mean(hetsd[,5])
 lbound<- hetmean - (sd*3)
 ubound<- hetmean + (sd*3)
-Stat<- c("SD2, "Lower Bound", "Uppder Bound", "Mean")
+Stat<- c("SD", "Lower Bound", "Upper Bound", "Mean")
 Values<- c(sd, lbound, ubound, hetmean)
 infin.3.sd<- data.frame(Stat, Values)
 write.table(infin.3.sd, file = "infin.3.sd.txt", row.names = F, quote = F, sep = "\t")
 q()
 
 ##Note upper and lower bound, named x and y respectively here. Remove heterozygosity outliers.
-'awk $5 <= y || $5 >= x' infin.3.prop.het.txt >> infin.3.het.drop.txt
+awk '$5 <= 0.319 || $5 >= 0.333' infin.awk '$5 <= 0.319 || $5 >= 0.333' infin.3.prop.het.txt >> infin.3.het.drop.txt
 plink --bfile infin.3.QC3 --remove infin.3.het.drop.txt --make-bed --out infin.3.QC4
