79dc38d1d7c71c77a8620ee1d0bd40d740bc3782
ecbridges <s1986958@ed.ac.uk>
2022-07-12 19:33:36 +0100

    corrections for infinium v1

--- 3.Infinium_550k_v1.1.sh
+++ 3.Infinium_550k_v1.1.sh
@@ -1,4 +1,5 @@
-#This is step 3 for the preparation of the Infinium 550k v1.1  array. This section will clean up
+plink --bfile infin.1.QC5 --split-x b37 no-fail --make-bed --out infin.1.QC6
+plink --bfile infin.1.QC6 --set-hh-missing --make-bed infin.1.QC7#This is step 3 for the preparation of the Infinium 550k v1.1  array. This section will clean up
 #the updated dataset by addressing any sex errors, related individuals and heterozygrous haploid
 #loci.
 
@@ -10,13 +11,16 @@
 plink --bfile infin.1.QC5 --split-x b37 no-fail --make-bed --out infin.1.QC6
 
 #Set remaining .hh instances to missing.
-plink --bfile infin.1.QC6 --set-hh-missing --make-bed infin.1.QC7
+plink --bfile infin.1.QC6 --set-hh-missing --make-bed --out infin.1.QC7
+
+#Set chromosome codes back for compatibility.
+plink --bfile infin.1.QC7 --merge-x --make-bed --out infin.1.QC8
 
 #Sex check on pruned dataset.
-plink --bfile infin.1.QC7 --indep-pairwise 1000 5 0.2 --out infin.1.QC7
-plink --bfile infin.1.QC7 --extract infin.1.QC7.prune.in --sex-check --out infin.1.sex
+plink --bfile infin.1.QC8 --indep-pairwise 1000 5 0.2 --out infin.1.QC8
+plink --bfile infin.1.QC8 --extract infin.1.QC8.prune.in --check-sex --out infin.1.sex
 grep PROBLEM infin.1.sex.sexcheck >> infin.1.sex.probs.txt
 
 #Remove individuals with sex errors.
-plink --brfile infin.1.QC7 --remove infin.1.sex.probs.txt --make-bed --out infin.1.QC8
+plink --bfile infin.1.QC8 --remove infin.1.sex.probs.txt --make-bed --out infin.1.QC9
 
--- 4.Infinium_550k_v1.1.sh
+++ 4.Infinium_550k_v1.1.sh
@@ -12,46 +12,31 @@
 #locating the outliers.
 #Follow this procedure to download the 1000 Genomes reference panel: https://meyer-lab-cshl.github.io/plinkQC/articles/Genomes1000.html
 #Follow this procedure to identify ancestry outliers: https://meyer-lab-cshl.github.io/plinkQC/articles/AncestryCheck.html
-#For all NCDS arrays, a theta of 3 was used rather than the default of 1.5, using the code below.
-
-R 
-library(plinkQC) 
-exclude_ancestry.infin.1 <-  evaluate_check_ancestry(indir="", name="",
-                          prefixMergedDataset=,
-                          europeanTh = 3,
-                          refSamplesFile=paste(indir=system.file(package = "plinkQC"), 
-"/extdata/Genomes1000_ID2Pop.txt",
-                                               sep=""),
-                          refColorsFile=paste(indir=system.file(package = "plinkQC"), 
-"/extdata/Genomes1000_PopColors.txt",
-                                              sep=""),
-                          interactive=TRUE)
-
-write.table(file = "infin.1.ancestry.txt", exclude_ancestry.infin.1$fail_ancestry, 
-	    row.names = F, quote = F)
+#For all NCDS arrays, a theta of 3 was used rather than the default of 1.5.
+#Place outliers in a file named infin.1.ancestry.txt
 
 #Remove ancestry outliers.
-plink --bfile infin.1.QC8 --remove infin.1.ancestry.txt --make-bed --out infin.1.QC9
+plink --bfile infin.1.QC9 --remove infin.1.ancestry.txt --make-bed --out infin.1.QC10plink --bfile infin.1.QC9 --remove infin.1.ancestry.txt --make-bed --out infin.1.QC10
 
 #At this stage you may carry out principal components analysis for ancestry on the dataset if you 
 #will be needing these values. 
-plink --bfile infin.1.QC9 --indep-pairwise 1000 5 0.2 --out infin.1.QC9
-plink --bfile infin.1.QC9 --extract infin.1.QC9.prune.in --make-bed --pca header tabs --out infin.1.pca
+plink --bfile infin.1.QC10 --indep-pairwise 1000 5 0.2 --out infin.1.QC10
+plink --bfile infin.1.QC10 --extract infin.1.QC10.prune.in --make-bed --pca header tabs --out infin.1.pca
 
 #Genetate allele frequencies from clean file.
-plink --bfile infin.1.QC9 --freq --out infin.1
+plink --bfile infin.1.QC10 --freq --out infin.1
 
 #Now check data using Will Rayner's checking script. Script and associated files are available here: 
 #	https://www.well.ox.ac.uk/~wrayner/tools/
 #Run using code:
-perl HRC-1000G-check-bim.pl -b infin.1.QC9.bim -f infin.1.frq -r HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h
+perl HRC-1000G-check-bim.pl -b infin.1.QC10.bim -f infin.1.frq -r HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h
 
 #Run the output script, Run-plink.sh, to clean the file.
 
 #Convert all files to VCF format
 for CHR in {1..23};
 do
-plink --bfile infin.1.QC9-updated-chr${CHR} --recode vcf --out infin.1.updated.chr${CHR}
+plink --bfile infin.1.QC10-updated-chr${CHR} --recode vcf --out infin.1.updated.chr${CHR}
 bcftools sort infin.1.updated.chr${CHR}.vcf -O z -o infin.1.chr${CHR}.vcf.gz
 done
 
