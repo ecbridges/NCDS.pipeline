1fd6f66396edf360e3f390a25096b3be14856c83
ecbridges <s1986958@ed.ac.uk>
2022-07-18 21:16:58 +0100

    Corrected error in 3.Affy_v6

--- 3.Affymetrix_v6.sh
+++ 3.Affymetrix_v6.sh
@@ -1,9 +1,3 @@
-link --bfile affy.6.QC9 --remove affy.6.related.excl.txt --make-bed --out affy.6.QC10
-plink --bfile affy.6.QC4 --split-x b37 no-fail --make-bed --out affy.6.QC5
-#This is step 3 for the preparation of the Illumina 1.2 million array. This section will clean up
-#the updated dataset by addressing any sex errors, related individuals and heterozygrous haploid
-#loci.
-
 #Requirements
 #	Plink 1.90b4
 #	Illumina 1.2 million array updated to build GRCh37 (name illumina.1.2.QC4)
